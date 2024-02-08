38ac3cd353c226f10fc2359d302fb3331ed96f36
Tokiwokoe <mam4ur.anastasia@gmail.com>
2024-02-08 19:07:36 +0300

    rabbitmq broker

--- bot/celeryconfig.py
+++ bot/celeryconfig.py
@@ -1,3 +1,3 @@
-broker_url = 'redis://localhost:6379/0'
+broker_url = 'amqp://admin:admin@localhost:5672/adminvhost'
 result_backend = 'redis://localhost:6379/0'
-broker_connection_retry_on_startup = False
+broker_connection_retry_on_startup = True
